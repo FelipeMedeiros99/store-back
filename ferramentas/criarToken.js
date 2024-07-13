811315b70afb22d018efc91a7484d8891104e8a8
Felipe <felipe.m.c.b.fm@gmail.com>
2024-07-13 15:51:32 -0300

    criação de token

---
+++ ferramentas/criarToken.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+/**
+ * retorna um token com base em um objeto inserido
+ * @param {Object} objeto - objeto que será encriptado
+ * @returns - token
+ */
+export default function criarToken(objeto){
+    dotenv.config()
+    const CHAVESECRETA = process.env.CHAVESECRETA
+    const token = jwt.sign(objeto, CHAVESECRETA)
+    return token
+}
