dc0030d90a7a9bbd53cd56b047e356d629fe6ca2
Felipe <felipe.m.c.b.fm@gmail.com>
2024-07-20 21:37:18 -0300

    enviando dados do usuário para o front

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import db from "../banco.js";
 
 export default async function loginController(req, res){
-    const {dadosUsuarioBanco, token} = req;
+    const {dadosUsuarioBanco, token, body} = req;
     dotenv.config();
     const PRODUTOSBANCO = process.env.PRODUTOSBANCO;
     let admin = false;
@@ -15,7 +15,9 @@ export default async function loginController(req, res){
         if(dadosUsuarioBanco.nome==="admin"){
             admin = true;
         }
-        res.status(200).send({produtos, admin, token});
+        delete dadosUsuarioBanco.senha;
+        delete dadosUsuarioBanco._id;
+        res.status(200).send({produtos, admin, token, dadosUsuarioBanco});
     }catch(e){
         res.status(404).send(`Erro ao efetuar login: ${e}`);
     }
